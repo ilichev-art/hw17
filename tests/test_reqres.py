cabbe49a4f00b59c3cec1d0f74782e5368eebc29
Artem Ilichev <artem.ilichev@list.ru>
2024-07-22 10:42:08 +0300

    add schemas

--- tests/test_reqres.py
+++ tests/test_reqres.py
@@ -32,11 +32,12 @@ def test_post_name_returns():
     assert response.status_code == 201
     assert body['name'] == name
 
-def test_get_single_user():
-    response = requests.get(f'{url}/users/2')
+def test_get_single_user_by_id():
+    id = '2'
+    response = requests.get(f'{url}/users/{id}')
     body = response.json()
 
-    assert body['data']['id'] == 2
+    assert body['data']['id'] == int(id)
     assert response.status_code == 200
     schema = schema_path("single_user.json")
     with open(schema) as file:
@@ -52,7 +53,8 @@ def test_put_update_last_name():
     assert boby['last_name'] == last_name
 
 def test_get_user_not_found():
-    response = requests.get(f'{url}/users/23')
+    id = '23'
+    response = requests.get(f'{url}/users/{id}')
 
     assert response.status_code == 404
     assert response.text == '{}'
@@ -62,6 +64,17 @@ def test_delete_user():
 
     assert response.status_code == 204
     assert response.text == ''
+
+def test_get_delay_response():
+    endpoint = '/users?delay=3'
+    response = requests.get(url + endpoint)
+    body = response.json()
+
+    assert response.status_code == 200
+    assert response.elapsed.total_seconds() <= 3.500000
+    schema = schema_path('users_with_delay.json')
+    with open(schema) as file:
+        validate(body, schema=json.loads(file.read()))
 
 def test_post_register_unsuccessful():
     payload = {'email': "sydney@fife"}
